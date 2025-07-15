82f9bd813ed44bfdc91e2289c09a8baf4bc1c874
Manas Malla <manasmalla.dev@gmail.com>
2025-07-15 14:52:20 +0530

    Initial Commit

---
+++ ChaosClinic/ChaosClinicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChaosClinicApp.swift
+//  ChaosClinic
+//
+//  Created by Manas Malla on 15/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ChaosClinicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChaosClinic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChaosClinic
+//
+//  Created by Manas Malla on 15/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
